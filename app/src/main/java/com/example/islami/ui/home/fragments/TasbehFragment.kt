29fa2b86b4f88819a5527055d3debf725ceaf9ac
Hend Sayed <hendsayed853@gmail.com>
2024-02-02 16:43:41 -0800

    The inital Project Added Splash Screen Added Home Screen With Fragment Added Quran Screen With Fragment Added Tasbeh Fragment Added Hadeth Fragment

--- app/src/main/java/com/example/islami/ui/home/fragments/TasbehFragment.kt
+++ app/src/main/java/com/example/islami/ui/home/fragments/TasbehFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import com.example.islami.R
 
 class TasbehFragment:Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
